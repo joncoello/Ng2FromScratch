2151ae1cb644ba17b6d34cf61f73f5629aa16a47
Jon Coello <joncoello@gmail.com>
2017-03-24 05:53:51 +0000

    get ng2 up and running

---
+++ Ng2FromScratch/app/app.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'main-app',
+    template: 'Hello Angular'
+})
+export class AppComponent {
+ 
+}
---
+++ Ng2FromScratch/app/app.module.ts
@@ -0,0 +1,20 @@
+﻿// entry point module
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+    imports: [
+        BrowserModule
+    ],
+    declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent],
+    providers: [
+        
+    ]
+})
+export class AppModule { }
